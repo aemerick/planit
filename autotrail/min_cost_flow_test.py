04709ff553f4db57b5d387e0761018952390995b
Andrew Emerick <aemerick11@gmail.com>
2020-09-21 19:56:55 -0700

    Working with my generated graph and using ortools compiled from source (finally)
    New function to generate the fake node needed to do this problem
    (ultimately) if start and end node are the same. Also sets up the
    supply list proplery for this usecase.

--- autotrail/min_cost_flow_test.py
+++ autotrail/min_cost_flow_test.py
@@ -1,7 +1,93 @@
 # """From Bradley, Hax, and Magnanti, 'Applied Mathematical Programming', figure 8.1."""
 
-from __future__ import print_function
-from ortools.graph import pywrapgraph
+
+#
+# Covering myself in case I screw up ortools install again
+#
+try:
+    # if installed from pip
+    from ortools.graph import pywrapgraph
+except:
+    # from source (ortools/ortools sub-directory)
+    from ortools.ortools.graph import pywrapgraph
+
+import numpy as np
+
+
+def set_nodes_and_supplies(node_list,start_node,end_node=None):
+    """
+    Given a list of node connections, adds an additional
+    'fake' node in the case that the start node and the end node
+    are the same. Returns the new node list and the source list.
+
+    Otherwise does not chage node list but does set the source list.
+
+    Parameters
+    ----------
+    node_list  : list of size 4 tuples
+                 List of node connections: [(start_node, end_node, capacity, cost),...]
+    start_node : (int) Start node number
+    end_node   : (int,optional) End node number. Assumed same as
+                 start node if set to None. Default : None
+
+    Returns
+    --------
+    node_list : list of size 4 tuples
+                List of node connections. Same as input list if start_node and
+                end_node are not the same.
+    supplies   : list
+                List of source values (1 at start_node, -1 at end node, zeros
+                elsewhere).
+    """
+
+    if len(node_list) == 1:
+        print("Only one node provided. Must have multiple nodes.")
+        raise ValueError
+
+    if end_node is None:
+        end_node = start_node
+
+    unique_start_nodes = np.unique(node_list[:,0])
+    unique_end_nodes   = np.unique(node_list[:,1])
+    unique_nodes       = np.unique([unique_start_nodes,unique_end_nodes])
+
+    if not (start_node in unique_start_nodes):
+        print("Start node not available in list ", start_node)
+        raise ValueError
+
+    if not (end_node in unique_end_nodes):
+        print("End node not available in list: ", end_node)
+        raise ValueError
+
+    fake_n = None # initialized for error catching
+    if end_node == start_node:
+        # need to add to list of nodes a new fake node that is an
+        # identical copy of the start node. It has the same connections
+        # but is not directly linked to the start node
+
+        fake_n = np.max(unique_nodes) + 1
+
+        nodes_to_add = []
+        for n in node_list:
+            if (n[0] == end_node):
+                nodes_to_add.append((fake_n, n[1], n[2], n[3]))
+            elif (n[1] == end_node):
+                nodes_to_add.append((n[0], fake_n, n[2], n[3]))
+
+        print(nodes_to_add)
+        node_list= np.insert(node_list,0, nodes_to_add, axis=0)
+
+        # updating for consistency
+        unique_start_nodes = np.insert(unique_start_nodes,0,fake_n)
+        unique_end_nodes = np.insert(unique_end_nodes,0,fake_n)
+        unique_nodes = np.insert(unique_nodes,0,fake_n)
+
+    # assign supplies. start is 1, end is -1, rest are 0
+    supplies = [0]*(np.max(unique_nodes)+1) # number of supplies is
+    supplies[start_node] = 1
+    supplies[end_node if (start_node!=end_node) else fake_n]   = -1
+
+    return node_list, supplies
 
 def main():
   """MinCostFlow simple interface example."""
@@ -10,14 +96,63 @@ def main():
   # between each pair. For instance, the arc from node 0 to node 1 has a
   # capacity of 15 and a unit cost of 4.
 
-  start_nodes = [ 0, 0,  1, 1,  1,  2, 2,  3, 4]
-  end_nodes   = [ 1, 2,  2, 3,  4,  3, 4,  4, 2]
-  capacities  = [15, 8, 20, 4, 10, 15, 4, 20, 5]
-  unit_costs  = [ 4, 4,  2, 2,  6,  1, 3,  2, 3]
+  #
+  # Need to generate a graph using star / end pairs for each
+  # direction possible (if one direction is not possible). This
+  # may be easiest to do and keep clean by making tuples and then
+  # stripping these into the individual lists. I *think* in my case
+  # I'll need to make the capacities = 1 (or same value...) and
+  # the unit costs = distance, elevation, etc.
+  #
+  # This seems to rely on all values being integers. SO pick a precision (0.01?)
+  # and work in units of 100's of miles (or mabe just use to nearest foot?)  #
+  # tuples are (start, end, capacity, cost)
+
+  node_paths = np.array([
+                      (0, 1, 1, 1),
+                      (1, 0, 1, 1),
+                      (1, 2, 1, 1),
+                      (1, 6, 1, 2),
+                      (2, 1, 1, 1),
+                      (2, 3, 1, 3),
+                      (2, 4, 1, 4),
+                      (2, 5, 1, 2),
+                      (3, 2, 1, 3),
+                      (4, 2, 1, 4),
+                      (4, 5, 1, 1),
+                      (5, 2, 1, 2),
+                      (5, 4, 1, 1),
+                      (5, 6, 1, 5),
+                      (6, 1, 1, 2),
+                      (6, 5, 1, 5) ])
+
+  # sets up the nodes to 
+  node_paths, supplies = set_nodes_and_supplies(node_paths, 0, 0)
+
+  start_nodes = node_paths[:,0].tolist()
+  end_nodes   = node_paths[:,1].tolist()
+  capacities  = node_paths[:,2].tolist() # should all be same!
+  unit_costs  = node_paths[:,3].tolist()
+
+
 
   # Define an array of supplies at each node.
+  #
+  #  1 is the start node
+  # -1 is the end node
+  #supplies = [1,
+#              0,
+#              0,
+#              0,
+#              -1,
+#              0,
+#              0]
 
-  supplies = [20, 0, 0, -5, -15]
+  print(start_nodes)
+  print(end_nodes)
+  print(capacities)
+  print(unit_costs)
+  print(supplies)
 
 
   # Instantiate a SimpleMinCostFlow solver.
