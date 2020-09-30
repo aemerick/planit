f2ba445aa9c4dc84da1591b6a3fbe3042526693b
Andrew Emerick <aemerick11@gmail.com>
2020-09-30 02:39:45 -0700

    comments and bugfixes. caching files by exact bbox coords

--- osm_data/osm_fetch.py
+++ osm_data/osm_fetch.py
@@ -14,13 +14,23 @@
 """
 
 import numpy as np
-import osmnx as ox
+import osmnx
+import os
 
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+#
 
 def get_graph(center_point = None,
               ll = None,
               rr = None,
-              dist=100000.0 # 100 km !
+              query=None,
+              dist=100000.0, # 100 km !
+              save_to_file=True,
+              allow_cache_load=True
               ):
     """
     A convenience wrapper around OSMNX's (already convenient) APIs to
@@ -30,39 +40,76 @@ def get_graph(center_point = None,
     If center is passed, computes the bounding box of width of the diameter
     of the circle nad uses bounding box computation.
 
+    This may take some time (5 - 60s) depending on the size of the region.
+
     Parameters:
     -----------
     center_point  : (optional) (lat,long) coordinate of center of map
     ll            : (optional) lower left corner (lat,long) of bounding box
     rr            : (optional) upper right corner (lat,long) of bounding box
+    query         : (optional) search query for doing graph_from_place. This
     dist          : (optional) radius of circle to draw
+    save_to_file  : (optional) Cache graph using lat long coordinats of bbox
+                        by pickling the graph object.
+    allow_cache_load : (optional) If bbox coordinate perfectly match a
+                          saved pickle on disk, allow to load this instead
 
     Returns:
     -----------
     ox_graph      : OSMNX Graph object of the selected region
-    
+
     """
     if (center_point is None) and (ll is None) and (rr is None):
         print("Must choose center coordinates or bounding box!")
         raise ValueError
     elif (ll is None) and (rr is None):
         # bad distance metric.. but this is approximate anyway
-        earth_c = 40.075E3 # km circumference
+        earth_c = 40.075E6 # km circumference
         sep     = dist / earth_c * 360.0
         ll = (center_point[0] - 0.5*sep, center_point[1] - 0.5*sep)
-        rr = (center_point[1] + 0.5*sep, center_point[1] + 0.5*sep)
+        rr = (center_point[0] + 0.5*sep, center_point[1] + 0.5*sep)
 
     elif (center_point is None):
         center_point = (0.5 * (ll[0]+rr[0]), 0.5 * (ll[1]+rr[1]))
     else:
         raise RuntimeError
 
-    north,south,east,west = rr[1],ll[1],rr[0],ll[0]
+    north,south,east,west = rr[0],ll[0],rr[1],ll[1]
 
+    call_api = True
+    if allow_cache_load:
+        inname = "%4.5f_%4.5f_%4.5f_%4.5f_osmnx_graph.pickle"%(north,south,east,west)
+        if os.path.isfile(inname):
+            with open(inname,'rb') as infile:
+                ox_graph = pickle.load(infile)
 
-    ox_graph = osmnx.graph_from_bbox(north,south,east,west,
-                          retain_all=True, truncate_by_edge=False,
-                          clean_periphery=True)
+            call_api = False
+
+    if call_api:
+        ox_graph = osmnx.graph_from_bbox(north,south,east,west,
+                              retain_all=True, truncate_by_edge=False,
+                              clean_periphery=True)
+
+        if save_to_file:
+        # pickle!!!!
+            outname = "%4.5f_%4.5f_%4.5f_%4.5f_osmnx_graph.pickle"%(north,south,east,west)
+
+            with open(outname,'wb') as outfile:
+                pickle.dump(ox_graph, outfile, protocol = 4)
 
 
     return ox_graph
+
+
+def test():
+    """
+    Test to make sure this works.
+    """
+
+    center_point = ( 34.2070, -118.13684)
+
+    ox_graph = get_graph(center_point=center_point, dist = 1.0E4)
+
+if __name__ == "__main__":
+
+    test()
